8c9db56decd011440b0456bb72e7e9fff1f6fe8a
chef-nic <nicholasajohnson@me.com>
2024-08-05 09:20:24 -0700

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Nicholas Johnson on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
