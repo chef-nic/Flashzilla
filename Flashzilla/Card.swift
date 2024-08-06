264b2ff40ac9b27ebba2032ad821e26f8635a271
chef-nic <nicholasajohnson@me.com>
2024-08-05 17:04:43 -0700

    flashcard app with accessibility

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Nicholas Johnson on 8/5/24.
+//
+
+import Foundation
+
+struct Card: Codable, Identifiable {
+    var id = UUID()
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Who played the 13th doctor in doctor who?", answer: "Jodie Whittaker")
+}
